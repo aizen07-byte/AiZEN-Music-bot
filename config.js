aaa7a3c11a480484658113342b2b2c18256220d9
aizen07-byte <this.ezz.sushant@gmail.com>
2025-02-21 19:42:21 +0545

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://sushant:<999@SusHant>@botbitch.cybiy.mongodb.net/?retryWrites=true&w=majority&appName=botbitch",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
